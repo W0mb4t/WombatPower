386d045c04fdca7922e30bf565cf6b452f1eba41
Unknown <AlixAkhribi@yahoo.com>
2017-11-15 12:49:15 -0800

    Added side nav for map api window

--- maps.js
+++ maps.js
@@ -239,3 +239,12 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
