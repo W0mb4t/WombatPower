d777151d9fafebb527157ea9bf382619ed23bf29
Leo <zenlionel@gmail.com>
2017-11-15 15:18:59 -0800

    Piped through to the front-end
    made it possible to fetch a large object from the server

--- assets/javascript/dataget.js
+++ assets/javascript/dataget.js
@@ -1,68 +1,34 @@
-$.getJSON('/data',function(response){
-console.log(response);
+$.getJSON('/data',function(data){
+console.log(data.bservices);
 });
-//display data on site
-//SMS06000005000000001
-//Statewide	Information
-// SMU06112445000000001
-// Anaheim-Santa Ana-Irvine, CA Metropolitan Division	Information	
-// SMU06125405000000001
-// Bakersfield, CA	Information	All Employees, In Thousands
-// SMU06170205000000001
-// Chico, CA	Information	All Employees, In Thousands
-// SMU06209405000000001
-// El Centro, CA	Information	
-// SMU06234205000000001
-// Fresno, CA	Information	
-// SMU06252605000000001
-// Hanford-Corcoran, CA	Information	
-// SMU06310805000000001
-// Los Angeles-Long Beach-Anaheim, CA	Information	
-// SMU06310845000000001
-// Los Angeles-Long Beach-Glendale, CA Metropolitan Division	Information	All Employees, In Thousands
-// SMU06314605000000001
-// Madera, CA	Information	
-// SMU06329005000000001
-// Merced, CA	Information	All Employees, In Thousands
-// SMU06337005000000001
-// Modesto, CA	Information
-// SMU06349005000000001
-// Napa, CA	Information	
-// SMU06360845000000001
-// Oakland-Hayward-Berkeley, CA Metropolitan Division	Information	
-// SMU06371005000000001
-// Oxnard-Thousand Oaks-Ventura, CA	Information	
-// SMU06398205000000001
-// Redding, CA	Information	
-// SMU06401405000000001
-// Riverside-San Bernardino-Ontario, CA	Information	
-// SMU06409005000000001
-// Sacramento--Roseville--Arden-Arcade, CA	Information
-// SMU06415005000000001
-// Salinas, CA	Information
-// SMU06417405000000001
-// San Diego-Carlsbad, CA	Information
-// SMU06418605000000001
-// San Francisco-Oakland-Hayward, CA	Information	
-// SMU06418845000000001
-// San Francisco-Redwood City-South San Francisco, CA Metropolitan Division	Information	
-// SMU06419405000000001
-// San Jose-Sunnyvale-Santa Clara, CA	Information	
-// SMU06420205000000001
-// San Luis Obispo-Paso Robles-Arroyo Grande, CA	Information	
-// SMU06420345000000001
-// San Rafael, CA Metropolitan Division	Information	
-// SMU06421005000000001
-// Santa Cruz-Watsonville, CA	Information	
-// SMU06422005000000001
-// Santa Maria-Santa Barbara, CA	Information	
-// SMU06422205000000001
-// Santa Rosa, CA	Information	
-// SMU06447005000000001
-// Stockton-Lodi, CA	Information
-// SMU06467005000000001
-// Vallejo-Fairfield, CA	Information	
-// SMU06473005000000001
-// Visalia-Porterville, CA	Information	
-// SMU06497005000000001
-// Yuba City, CA	Information	
+
+// SMS06000000600000001
+// Statewide	Goods Producing	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000001000000001
+// Statewide	Mining and Logging	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000003000000001
+// Statewide	Manufacturing	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000004200000001
+// Statewide	Retail Trade	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000004300000001
+// Statewide	Transportation, Warehousing, and Utilities	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000005000000001
+// Statewide	Information	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000005552000001
+// Statewide	Finance and Insurance	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000005553000001
+// Statewide	Real Estate and Rental and Leasing	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000006000000001
+// Statewide	Professional and Business Services	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000006054000001
+// Statewide	Professional, Scientific, and Technical Services	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000006055000001
+// Statewide	Management of Companies and Enterprises	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000006500000001
+// Statewide	Education and Health Services	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000007071000001
+// Statewide	Arts, Entertainment, and Recreation	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000007072000001
+// Statewide	Accommodation and Food Services	All Employees, In Thousands	Seasonally Adjusted
+// SMS06000009000000001
+// Statewide	Government	All Employees, In Thousands	Seasonally Adjusted
--- maps.js
+++ maps.js
@@ -19,39 +19,81 @@ app.use(express.static('assets'))
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/data", function (req, res) {
-  for (var i = 0; i < dataArr.length; i++) {
-    dataArr[i];
-    console.log(dataArr[i]);
-    axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/' + dataArr[i] + '?registrationkey=4b920f31441640c28f78a718782d5f6e')  
-    .then(function (response) {
-      var newArr = [];
-      newArr.push(response);
-        console.log(newArr);
-        res.json(newArr[1]);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.status(500).send({
-          error: 'Something\'s not quite right'
-        });
+
+  //leo api key= a4faaa6f019c42eabd6ba809c57f1c85
+  //roy api key= 4b920f31441640c28f78a718782d5f6e
+  //bruteforced through this in order to get the data. Ideally create a for loop to populate the area with the seriesID
+  Promise.all([
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000000600000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000001000000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000003000000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000004200000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000004300000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000005000000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000005552000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000005553000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000006000000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000006054000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000006055000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000006500000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000007071000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000007072000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85'),
+      axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/SMS06000009000000001?registrationkey=a4faaa6f019c42eabd6ba809c57f1c85')
+
+    ]).then(function (response) {
+      //bruteforced here to create a new object from the array that was returned in the response, this way we can send it 
+      //to the frontend. This let's us manipulate the giant object to get the data we want.
+      console.log('RESPONSE', response);
+      var goods = response[0].data.Results.series[0];
+      var mining = response[1].data.Results.series[0];
+      var manuf = response[2].data.Results.series[0];
+      var retail = response[3].data.Results.series[0];
+      var twu = response[4].data.Results.series[0];
+      var infor = response[5].data.Results.series[0];
+      var finance = response[6].data.Results.series[0];
+      var estate = response[7].data.Results.series[0];
+      var bservices = response[8].data.Results.series[0];
+      var tservices = response[9].data.Results.series[0];
+      var manage = response[10].data.Results.series[0];
+      var education = response[11].data.Results.series[0];
+      var entertain = response[12].data.Results.series[0];
+      var food = response[13].data.Results.series[0];
+      var govt = response[14].data.Results.series[0];
+      
+      var newResponse = {
+        goods: goods,
+        mining: mining,
+        manuf: manuf,
+        retail: retail,
+        twu: twu,
+        infor: infor,
+        finance: finance,
+        estate: estate,
+        bservices: bservices,
+        tservices: tservices,
+        manage: manage,
+        education: education,
+        entertain: entertain,
+        food: food,
+        govt: govt
+        
+      };
+      console.log(newResponse);
+      res.json(newResponse);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(500).send({
+        error: 'Something\'s not quite right'
       });
-  }
-
+    });
 });
-// app.get("/add", function(req, res) {
-//   res.sendFile(path.join(__dirname, "add.html"));
-// });
 
-// app.get("/all", function(req, res) {
-//   res.sendFile(path.join(__dirname, "all.html"));
-// });
-
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
