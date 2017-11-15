98d39300082558de32a2dc7e47506cfcc6806f2f
Unknown <AlixAkhribi@yahoo.com>
2017-11-15 14:38:10 -0800

    Added button connected to side nav bar

--- maps.js
+++ maps.js
@@ -239,6 +239,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 
+
 /* Open the sidenav */
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
