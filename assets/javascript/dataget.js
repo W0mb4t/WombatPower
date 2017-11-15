81bb29ba8947e70bff7f4179dc78296988611c67
Leo <zenlionel@gmail.com>
2017-11-15 08:34:24 -0800

    attempted for loop with data storage
    gogogadget fail

--- assets/javascript/dataget.js
+++ assets/javascript/dataget.js
@@ -1,3 +1,68 @@
-$.getJSON('/data',function(data){
-console.log(data);
+$.getJSON('/data',function(response){
+console.log(response);
 });
+//display data on site
+//SMS06000005000000001
+//Statewide	Information
+// SMU06112445000000001
+// Anaheim-Santa Ana-Irvine, CA Metropolitan Division	Information	
+// SMU06125405000000001
+// Bakersfield, CA	Information	All Employees, In Thousands
+// SMU06170205000000001
+// Chico, CA	Information	All Employees, In Thousands
+// SMU06209405000000001
+// El Centro, CA	Information	
+// SMU06234205000000001
+// Fresno, CA	Information	
+// SMU06252605000000001
+// Hanford-Corcoran, CA	Information	
+// SMU06310805000000001
+// Los Angeles-Long Beach-Anaheim, CA	Information	
+// SMU06310845000000001
+// Los Angeles-Long Beach-Glendale, CA Metropolitan Division	Information	All Employees, In Thousands
+// SMU06314605000000001
+// Madera, CA	Information	
+// SMU06329005000000001
+// Merced, CA	Information	All Employees, In Thousands
+// SMU06337005000000001
+// Modesto, CA	Information
+// SMU06349005000000001
+// Napa, CA	Information	
+// SMU06360845000000001
+// Oakland-Hayward-Berkeley, CA Metropolitan Division	Information	
+// SMU06371005000000001
+// Oxnard-Thousand Oaks-Ventura, CA	Information	
+// SMU06398205000000001
+// Redding, CA	Information	
+// SMU06401405000000001
+// Riverside-San Bernardino-Ontario, CA	Information	
+// SMU06409005000000001
+// Sacramento--Roseville--Arden-Arcade, CA	Information
+// SMU06415005000000001
+// Salinas, CA	Information
+// SMU06417405000000001
+// San Diego-Carlsbad, CA	Information
+// SMU06418605000000001
+// San Francisco-Oakland-Hayward, CA	Information	
+// SMU06418845000000001
+// San Francisco-Redwood City-South San Francisco, CA Metropolitan Division	Information	
+// SMU06419405000000001
+// San Jose-Sunnyvale-Santa Clara, CA	Information	
+// SMU06420205000000001
+// San Luis Obispo-Paso Robles-Arroyo Grande, CA	Information	
+// SMU06420345000000001
+// San Rafael, CA Metropolitan Division	Information	
+// SMU06421005000000001
+// Santa Cruz-Watsonville, CA	Information	
+// SMU06422005000000001
+// Santa Maria-Santa Barbara, CA	Information	
+// SMU06422205000000001
+// Santa Rosa, CA	Information	
+// SMU06447005000000001
+// Stockton-Lodi, CA	Information
+// SMU06467005000000001
+// Vallejo-Fairfield, CA	Information	
+// SMU06473005000000001
+// Visalia-Porterville, CA	Information	
+// SMU06497005000000001
+// Yuba City, CA	Information	
--- maps.js
+++ maps.js
@@ -7,6 +7,7 @@ var axios = require("axios");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+var dataArr = ['SMU06112445000000001', 'SMU06125405000000001', 'SMU06170205000000001', 'SMU06209405000000001', 'SMU06234205000000001', 'SMU06252605000000001', 'SMU06310805000000001', 'SMU06310845000000001', 'SMU06314605000000001', 'SMU06329005000000001', 'SMU06337005000000001', 'SMU06349005000000001', 'SMU06360845000000001', 'SMU06371005000000001', 'SMU06398205000000001', 'SMU06401405000000001', 'SMU06409005000000001', 'SMU06415005000000001', 'SMU06417405000000001', 'SMU06418605000000001', 'SMU06418845000000001', 'SMU06419405000000001', 'SMU06420205000000001', 'SMU06420345000000001', 'SMU06421005000000001', 'SMU06422005000000001', 'SMU06422205000000001', 'SMU06447005000000001', 'SMU06467005000000001', 'SMU06473005000000001', 'SMU06497005000000001']
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({
@@ -22,15 +23,24 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/data", function (req, res) {
-  axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/APU0000702421?registrationkey=4b920f31441640c28f78a718782d5f6e')
+  for (var i = 0; i < dataArr.length; i++) {
+    dataArr[i];
+    console.log(dataArr[i]);
+    axios.post('https://api.bls.gov/publicAPI/v2/timeseries/data/' + dataArr[i] + '?registrationkey=4b920f31441640c28f78a718782d5f6e')  
     .then(function (response) {
-      console.log(response);
-      res.json(response.data.Results.series[0].data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.status(500).send({error:'Something\'s not quite right'});
-    });
+      var newArr = [];
+      newArr.push(response);
+        console.log(newArr);
+        res.json(newArr[1]);
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.status(500).send({
+          error: 'Something\'s not quite right'
+        });
+      });
+  }
+
 });
 // app.get("/add", function(req, res) {
 //   res.sendFile(path.join(__dirname, "add.html"));
